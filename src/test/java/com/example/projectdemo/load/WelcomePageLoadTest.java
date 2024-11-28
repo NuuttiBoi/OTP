c1e0ea1d6565b52f17d4991ba7913af290d4f649
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2024-11-29 01:35:48 +0200

    testing language selection automatically

---
+++ src/test/java/com/example/projectdemo/load/WelcomePageLoadTest.java
@@ -0,0 +1,46 @@
+package com.example.projectdemo.load;
+
+import com.example.projectdemo.model.LanguageOption;
+import javafx.fxml.FXMLLoader;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import org.testfx.api.FxAssert;
+import static org.testfx.matcher.base.NodeMatchers.isNotNull;
+
+class WelcomePageLoadTest extends ApplicationTest {
+
+    private ComboBox<LanguageOption> languageSelection;
+    private Button signInButton;
+    private Text welcomeText;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/projectdemo/fxmlFiles/Welcome.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 330, 600);
+        stage.setTitle("Car Rental");
+        stage.setScene(scene);
+        stage.show();
+
+        languageSelection = lookup("#languageSelection").query();
+        signInButton = lookup("#signInButton").query();
+        welcomeText = lookup("#welcomeText").query();
+
+        FxAssert.verifyThat(languageSelection, isNotNull());
+        FxAssert.verifyThat(signInButton, isNotNull());
+        FxAssert.verifyThat(welcomeText, isNotNull());
+    }
+
+    @Test
+    void testUserInteraction() {
+        clickOn(languageSelection); // Open language dropdown
+        clickOn("English"); // Select English language
+        clickOn(signInButton); // Click on the sign-in button
+
+        FxAssert.verifyThat(welcomeText, isNotNull());
+    }
+}
